b6b22325e78e920372dd5224a1343e9e01c2c510
Temur Radjabov <t.radjabov@andersenlab.com>
2022-08-02 15:52:57 +0300

    end

---
+++ js/swipper.js
@@ -0,0 +1,35 @@
+$(function () {
+  $(".multiple-items").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    centerMode: true,
+    centerPadding: "0",
+    arrows: false,
+    initialSlide: 1,
+    responsive:[
+      {
+        breakpoint: 1024,
+        settings:{
+          slidesToShow: 2,
+          centerMode: false,
+          
+        }
+      },
+      {
+        breakpoint: 768,
+        settings:{
+          slidesToShow: 1,
+          centerMode: false,
+
+        }
+      }
+    ]
+ /*    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnFocus: true,
+    pauseOnHover: true, 
+    pauseOnDotsHover: true */
+  });
+});
